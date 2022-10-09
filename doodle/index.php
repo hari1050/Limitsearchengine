059c36f9117dc9f824d718b2eede71d6a9118644
unitx2 <103807078+unitx2@users.noreply.github.com>
2022-10-09 12:35:20 +0530

    Add files via upload

---
+++ doodle/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to doodle</title>
+
+	<meta name="description" content="Searcg the web for sites and images.">
+	<meta name="keywords" content="Search engine, doodle, websites">
+	<meta name="author" content="Hari">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<div class="wrapper indexPage">
+
+
+		<div class="mainSection">
+
+			<div class="logoContainer">
+				<img src="assets/images/doodle_logo.png" title="Logo" alt="Site logo">
+			</div>
+
+			<div class="searchContainer">
+
+				<form action="search.php" method="GET">
+					
+					<input class="searchBox" type="text" name="term">
+					<input class="searchButton" type="submit" value="Search">
+
+
+				</form>
+
+
+			</div>
+
+
+			
+		</div>
+
+
+
+	</div>
+
+</body>
+</html>
